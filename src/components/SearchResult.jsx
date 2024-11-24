a35bf0e7c4bd2cdb7e272c9f847149d4ba16a6b9
kartikbanga <kartikbanga04@gmail.com>
2024-11-25 02:51:09 +0530

    resolving errors

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -56,6 +56,7 @@ const SearchResult = () => {
                             />
                         );
                         }
+                        return null;
                     })}
                 </div>
             </div>
